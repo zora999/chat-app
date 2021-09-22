cc405af8ab6bcfdfcb8f5d4cd63e517ea81b4fe2
zora999 <zora@mua.biglobe.ne.jp>
2021-09-22 13:10:31 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 
 end
