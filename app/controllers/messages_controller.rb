76b3b7e309ace33761423ac4c920711dba453a2f
zora999 <zora@mua.biglobe.ne.jp>
2021-09-24 12:29:15 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
   def create
     @room = Room.find(params[:room_id])
@@ -9,6 +10,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
